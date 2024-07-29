a72fefb65e643d668f7e9faaeece9fa99f70799a
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2024-07-29 16:44:56 +0900

    test

--- files/twipro_quiz_data.js
+++ files/twipro_quiz_data.js
@@ -1,6 +1,18 @@
-function TwiproData() {
+const word_dic = {
+    '과제' : '課題'
+}
+
+function wordQuiz(){
+    for(let key in word_dic){
+        console.log(key);
+        console.log(word_dic[key]);
+    }
     return [
         new quizObject("과제の意味は？", ["価値", "課題", "内容"], 1),
         new quizObject("Test", ["a", "b", "c"], 0)
     ];
+}
+
+function TwiproData() {
+    return wordQuiz();
 }
